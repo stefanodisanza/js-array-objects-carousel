865e8abcd881ddcf041619e4cda2d76952c9387b
stefanodisanza <disanzastefano@gmail.com>
2023-02-15 16:00:31 +0100

    FEAT carosello

---
+++ app.js
@@ -0,0 +1,17 @@
+const slides = document.querySelectorAll(".slide");
+console.log(slides)
+      let currentSlide = 0;
+
+      document.querySelector(".next").addEventListener("click", function() {
+        slides[currentSlide].classList.remove("active");
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].classList.add("active");
+      });
+
+      document.querySelector(".prev").addEventListener("click", function() {
+        slides[currentSlide].classList.remove("active");
+        currentSlide = (currentSlide + slides.length - 1) % slides.length;
+        slides[currentSlide].classList.add("active");
+      });
+
+     
